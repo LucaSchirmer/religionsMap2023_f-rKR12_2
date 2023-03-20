c85e8b9fb48fcd78200596f53169d5a95cd08c1e
Luca Schirmer <acu1410l@gmail.com>
2023-03-20 17:55:30 +0100

    Update sketch.js

--- website/sketch.js
+++ website/sketch.js
@@ -12,27 +12,59 @@ async function getImg(url){
     return response.json();
 }
 
-
+let bool = true;
 async function setup(){
     createCanvas(floor(1952), floor(screenY / 1.25));
     pixelDensity(1);
     pixelArray = await getImg("../img.json");
     
     background(1);
-    
-}
 
-let bool = true;
-function draw(){
-    do {
+
+    document.querySelector('#defaultCanvas0').addEventListener("click", c =>{
+        let mX = mouseX;
+        let mY = mouseY;
+
+        let pixel = [];
+        loadPixels();
+
+        pixel.push((floor(mX) + floor(mY)  * width) * 4);
+        pixel.push(pixels[pixel[0]]);
+        pixel.push(pixels[pixel[0]+1]);
+        pixel.push(pixels[pixel[0]+2]);
+        pixel.push(pixels[pixel[0]+3]);
+        
+        
+        console.log(pixel);
+
+        switch (true){
+            case pixel[1] < 110 && pixel[2] > 150 && pixel[3] < 110:
+
+            console.log("GREEEN");
+            break;
+            case pixel[1] < 30 && pixel[2] > 80 && pixel[3] < 70:
+
+            console.log("GREEEN");
+            break;
+        }
+
+        console.log(mX, mY)
+        let location = window.location.href;
+        let url = new URL(location.replace("index.html", "info.html")); 
+
+        let parameter = new URLSearchParams(url.search);
+    
+
+        parameter.append("searchh", "g");
+        console.log(url)
+    });
+
         loadPixels();
         if(pixelArray){
             console.log(pixelArray)
-  
             for (let y = 0; y <  1072; y++){
                 for (let x = 0; x < 1952; x++){
                     let index = (x + y  * width) * 4; 
-                    console.log(index)
                     pixels[index] = pixelArray.arrayOne[y][x][0];
                     pixels[index+1] = pixelArray.arrayOne[y][x][1];
                     pixels[index+2] = pixelArray.arrayOne[y][x][2];
@@ -43,19 +75,10 @@ function draw(){
             bool = false;
         }
         updatePixels();
-    } while (bool);
     
-        
 }
 
-   
 
+function draw(){
 
-document.querySelector('#defaultCanvas0').addEventListener("click", c =>{
-    mX = MouseX;
-    mY = MouseY;
-
-    let url = new URL("${window.location}/info.html");
-
-
-});
+}
